e77e81b896838ccfe4bbb68d41fbaab28283f7f4
Gregory Reyes <greyes3@udi.edu.co>
2023-08-20 19:38:59 -0500

    Implementación de migraciones de DB

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models.account import Account
+from .models.user import User
+from authApp.models import user
+
+from authApp.models import account
 
 # Register your models here.
+admin.site.register(User) #pilas puesssssssssssssssssssss con la mayuscula inicial
+admin.site.register(Account) # tambien
